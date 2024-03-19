90ca93b5abc1c6dfda3d13bcc99735fc8d291a4b
TamNhien <canhthien91@gmail.com>
2024-03-19 19:09:14 +0700

    Add project files.

---
+++ P_XuatMaxMin.py
@@ -0,0 +1,8 @@
+﻿def main():
+    a, b, c = map(float, input("Nhập ba số a, b, c: ").split())
+    max_value, min_value = max(a, b, c), min(a, b, c)
+    print(f"Giá trị lớn nhất là : {max_value}\nGiá trị nhỏ nhất là : {min_value}")
+
+if __name__ == "__main__":
+    main()
+
